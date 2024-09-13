32b4f7d92b30515f0f3fa4ce8653d5e464a89c90
Daylan Maharaj <maharaj.daylan@gmail.com>
2024-09-12 20:56:33 -0400

    Snake Game --> Checks for collisions

--- main.py
+++ main.py
@@ -36,14 +36,85 @@ class Food:
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOUR, tag="food")
       
 
-def next_turn():
-    pass  # Implement snake movement and food collision
+def next_turn(snake, food):
+
+    x, y = snake.coordinates[0]
+
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
+
+    snake.coordinates.insert(0, (x, y))
+
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOUR)
+    snake.squares.insert(0, square)
+
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+        global score
+        score += 1
+        label.config(text="Score:{}".format(score))
+
+        canvas.delete("food")
+        food = Food()  # Create new food
+    else:
+        del snake.coordinates[-1]
+        canvas.delete(snake.squares[-1])
+        del snake.squares[-1]
+
+    if check_collisions(snake):
+        game_over()
+    else:        
+
+        window.after(SPEED, next_turn, snake, food)
+    
+
+   
 
 def change_direction(new_direction):
-    pass  # Implement direction change logic
+    global direction
 
-def check_collisions():
-    pass  # Check for collisions with walls or the snake itself
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+
+    if new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+
+    if new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+
+    if new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
+                    
+                    
+
+
+
+
+def check_collisions(snake):
+
+    x, y = snake.coordinates[0]
+
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+    
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+        
+    return False    
+    
 
 def game_over():
     pass  # Handle the end of the game
@@ -80,8 +151,16 @@ y = int((screen_height / 2) - (window_height / 2))
 # Now set the geometry to center the window
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+
 snake = Snake()
 food = Food()
+
+next_turn(snake, food)
 
 # Start the main loop
 window.mainloop()
